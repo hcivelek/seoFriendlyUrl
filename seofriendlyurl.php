c51b79e03f5fbbc5e5f94410b20e6a565b59a542
Hasan <hcivelek@kuzen.net>
2014-05-25 02:17:13 +0300

    Update seofriendlyurl.php

--- seofriendlyurl.php
+++ seofriendlyurl.php
@@ -2,11 +2,11 @@
     
     function seoFriendlyUrl($string)
     {
-		  setlocale(LC_CTYPE, 'en_US.UTF8');
- 		  $string = iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', trim($string));
- 	  	$string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens. 		
- 		  $string=strtolower(preg_replace('/[^A-Za-z0-9\-]/', '', $string)); // Removes special chars.
- 		  return preg_replace('/[-]+/','-',$string);	    
+	setlocale(LC_CTYPE, 'en_US.UTF8');
+	$string = iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', trim($string));
+ 	$string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens. 		
+	$string=strtolower(preg_replace('/[^A-Za-z0-9\-]/', '', $string)); // Removes special chars.
+	return preg_replace('/[-]+/','-',$string);	    
     }
     
     $basliklar[]="Merhaba Dünya, Al sana sıradan bir Türkçe Başlık";
